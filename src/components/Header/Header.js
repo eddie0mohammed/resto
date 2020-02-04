b363d331fd43c70e5f06224c78ee2ac3150110a3
eddie0mohammed <me3dun@gmail.com>
2020-02-04 14:25:39 +0400

    worked on responsive part1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,8 +18,7 @@ class Header extends React.Component{
                     <NavLink to='/' exact activeClassName={styles.selected} className={styles.navlinks}>Home</NavLink>
                     <NavLink to="/about" exact activeClassName={styles.selected} className={styles.navlinks}>About</NavLink>
                     <NavLink to="/menu" exact  activeClassName={styles.selected} className={styles.navlinks}>Menu</NavLink>
-                    {/* <NavLink to='/gallery' exact activeClassName={styles.selected}  className={styles.navlinks}>Gallery</NavLink> */}
-                    {/* <NavLink to='/reservations' exact activeClassName={styles.selected}  className={styles.navlinks}>Reservations</NavLink> */}
+                    
 
                 </nav>
                 <button className={styles.reservations} onClick={this.props.modalHandler}>Reservations</button>
--- src/components/ReservationSection/ReservationSection.js
+++ src/components/ReservationSection/ReservationSection.js
@@ -12,19 +12,21 @@ class ReservationSection extends React.Component{
         return (
             <div className={styles.reservationSection}>
 
-                <div className={styles.left}>
-                    <p className={styles.story}>Our story</p>
-                    <h2 className={styles.header}>Cooking is the art of adjustment</h2>
-                    <p className={styles.description}>Do right and far exceed expectations daily. We do the right thing and lead by putting people first. Each and every connection with our team, guests and shareholders is our opportunity to create an experience that FEEDs – Far Exceeds Expectations Daily – and show how much we care.</p>
-                    
-                    <p className={styles.who}><span className={styles.chef}>Thomas Eggsy</span> - Master Chef</p>
+                <div className={styles.container}>
+                    <div className={styles.left}>
+                        <p className={styles.story}>Our story</p>
+                        <h2 className={styles.header}>Cooking is the art of adjustment</h2>
+                        <p className={styles.description}>Do right and far exceed expectations daily. We do the right thing and lead by putting people first. Each and every connection with our team, guests and shareholders is our opportunity to create an experience that FEEDs – Far Exceeds Expectations Daily – and show how much we care.</p>
+                        
+                        <p className={styles.who}><span className={styles.chef}>Thomas Eggsy</span> - Master Chef</p>
 
-                    <button className={styles.reservation} onClick={this.props.showModal}>Reservation</button>
-                </div>
+                        <button className={styles.reservation} onClick={this.props.showModal}>Reservation</button>
+                    </div>
 
-                <div className={styles.right}>
-                    <div className={styles.overlay}>&nbsp;</div>
-                    <img className={styles.img} src="https://source.unsplash.com/jeiqzOgwwKU" alt="reservation"/>
+                    <div className={styles.right}>
+                        <div className={styles.overlay}>&nbsp;</div>
+                        <img className={styles.img} src="https://source.unsplash.com/jeiqzOgwwKU" alt="reservation"/>
+                    </div>
                 </div>
 
             </div>
